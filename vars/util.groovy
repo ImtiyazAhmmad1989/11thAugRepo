c28a41ed5a011c873b1a3c0a0662d29e4e5c58e9
Imtiyaz Ahmmad Avati <42310129+ImtiyazAhmmad1989@users.noreply.github.com>
2021-09-27 19:35:15 +0530

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -2,9 +2,9 @@ import java.util.List
 import org.apache.commons.lang3.text.StrSubstitutor
 
 
-def m1(p,params){
+def m1(parent,params){
 
-println WELCOME_MESSAGE
+println parent.WELCOME_MESSAGE
 
 println '******PRINTING ALL ITEMS ******'
 
