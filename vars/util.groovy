b737874f011ce200e5c0559a459168a0c08ee714
Imtiyaz Ahmmad Avati <42310129+ImtiyazAhmmad1989@users.noreply.github.com>
2021-09-27 19:28:57 +0530

    Update util.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -8,16 +8,8 @@ def m1(params){
 println '******PRINTING ALL ITEMS ******'
 	
 	
-for (param in params) {
-        String[] paramArray = param.split(":", 2)
-        String key = paramArray[0]
-        if (paramArray.length == 2) {
-            String value = paramArray[1]
-            println value
-            parent.parameterMap.put(key.toString().trim(), value.toString().trim())
-        } else if (paramArray.length == 1) {
-            parent.parameterMap.put(key.toString().trim(), "")
-        }
+for (p in params) {
+       println p
 
 }
 }
